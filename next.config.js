592f837c83ca8db10d1fd61025f31abdc461c2f1
Suraj Singhania <surajsinghania82@gmail.com>
2025-01-14 18:31:37 +0530

    added build folder to output

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  distDir: 'build', // This will tell Next.js to output the build files to the 'build' folder
 };
 
 module.exports = nextConfig;

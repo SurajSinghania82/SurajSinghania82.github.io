1c78cebb993bef16b30d55895f9b3d95d303f430
Suraj Singhania <surajsinghania82@gmail.com>
2025-01-14 18:58:54 +0530

    modified nextjs config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
   },
   images: { unoptimized: true },
   distDir: 'build', // This will tell Next.js to output the build files to the 'build' folder
+  basePath: '',
+  assetPrefix: '',
+  // Optional: Enable static optimization for specific pages
+  trailingSlash: true, // Creates index.html in the `out/` directory for each page
 };
 
 module.exports = nextConfig;
